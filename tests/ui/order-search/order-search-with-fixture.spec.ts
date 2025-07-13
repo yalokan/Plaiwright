9e2648ef56f27a2b73429431fba331d28c822612
vladimirovodenko <vladimir@tallinn-learning.ee>
2025-07-13 22:52:57 +0300

    enhance order search test to validate order status colors

--- tests/ui/order-search/order-search-with-fixture.spec.ts
+++ tests/ui/order-search/order-search-with-fixture.spec.ts
@@ -2,10 +2,18 @@ import { SERVICE_URL } from '../../../config/env-data'
 import { test } from '../../fixtures/general.fixture'
 import { expect } from '@playwright/test'
 
-test.only('search for existing order', async ({ pageJwt, orderId }) => {
+test('search for existing order', async ({ pageJwt, orderId }) => {
   await pageJwt.goto(SERVICE_URL)
   await pageJwt.getByTestId('openStatusPopup-button').click()
   await pageJwt.getByTestId('searchOrder-input').fill(String(orderId))
   await pageJwt.getByTestId('searchOrder-submitButton').click()
-  await expect(pageJwt.getByText('OPEN')).toBeVisible()
+
+  const orderOpenStatusColor = await pageJwt
+    .getByText('OPEN')
+    .evaluate((el) => getComputedStyle(el).backgroundColor)
+  expect.soft(orderOpenStatusColor).toBe('rgb(253, 204, 0)')
+  const orderDeliveredStatusColor = await pageJwt
+    .getByText('DELIVERED', { exact: true })
+    .evaluate((el) => getComputedStyle(el).backgroundColor)
+  expect.soft(orderDeliveredStatusColor).toBe('rgba(0, 0, 0, 0)')
 })
