4e5f16a66b695d1319ab123a2224515029e4b8f3
MaxSt <mstekolstsikov@evolution.com>
2025-07-14 14:53:48 +0300

    Remove ONLY in test

--- tests/ui/auth/auth-page-object.spec.ts
+++ tests/ui/auth/auth-page-object.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 import { LoginPage } from '../../pages/login-page'
 
-test.only('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
+test('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
   const authPage = new LoginPage(page)
   await authPage.open()
   const orderPage = await authPage.signIn()
